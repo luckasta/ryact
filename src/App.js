2363dd93cf766b2c88a1dbd4b329a669aae26893
cafeshine23@gmail.com <lucianaveraaa2008@gmail.com>
2025-07-28 15:46:26 -0500

    ados

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import Saludo from "./Saludo";
+import Contador from "./Contador";
 function App() {
   return (
     <div>
       <h1>¡Buenas React!</h1>
       <p>Este es mi primer componente.</p>
 
-      <Saludo nombre="Luck"/>
+      <Saludo nombre="Santi"/>
+      <Saludo nombre="Lauri"/>
+
+      <Contador />
     </div>
   );
 }
---
+++ src/Contador.js
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+function Contador() {
+  const [contador, setContador] = useState(0);
+
+  return (
+    <div>
+      <h2>Contador: {contador}</h2>
+      <button onClick={() => setContador(contador + 1)}>Aumentar</button>
+    </div>
+  );
+}
+
+export default Contador;
+
